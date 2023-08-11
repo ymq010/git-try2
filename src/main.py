c3816ed0e9be49bb0de166804b6c7f9d95960adb
wr10 <y01814318@stu.ahu.edu.cn>
2023-08-11 19:20:52 +0800

    one more

--- src/main.py
+++ src/main.py
@@ -26,13 +26,13 @@ parser.add_argument('--using_all_hops', type=bool, default=True,
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='book', help='which dataset to use')
 parser.add_argument('--dim', type=int, default=4, help='dimension of entity and relation embeddings')
-parser.add_argument('--n_hop', type=int, default=2, help='maximum hops')
+parser.add_argument('--n_hop', type=int, default=4, help='maximum hops')
 parser.add_argument('--kge_weight', type=float, default=1e-2, help='weight of the KGE term')
 parser.add_argument('--l2_weight', type=float, default=1e-5, help='weight of the l2 regularization term')
 parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
 parser.add_argument('--batch_size', type=int, default=1024, help='batch size')
 parser.add_argument('--n_epoch', type=int, default=10, help='the number of epochs')
-parser.add_argument('--n_memory', type=int, default=16, help='size of ripple set for each hop')
+parser.add_argument('--n_memory', type=int, default=8, help='size of ripple set for each hop')
 parser.add_argument('--item_update_mode', type=str, default='plus_transform',
                     help='how to update item at the end of each hop')
 parser.add_argument('--using_all_hops', type=bool, default=True,
